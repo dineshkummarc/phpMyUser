c70892543efed1180429cbddb6aa9fc16fc48e5f
TheEarl <TheEarl1224@googlemail.com>
2014-06-19 18:18:39 +0200

    Rename modals

---
+++ modals.php
@@ -0,0 +1,74 @@
+<div class="modal fade login-form" tabindex="-1">
+	<div class="modal-dialog modal-sm">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1 class="">Login</h1>
+			</div>
+			<div class="modal-body">
+				<h5>
+					<div class="input-group">
+						<span class="input-group-addon">
+						   <i class="fa fa-user fa-fw"></i>
+						</span>
+						<input type="text" placeholder="Username" class="form-control login-username">
+					</div>
+				</h5>
+				<h5>
+					<div class="input-group">
+						<span class="input-group-addon">
+						   <i class="fa fa-key fa-fw"></i>
+						</span>
+						<input type="password" placeholder="Password" class="form-control login-password">
+						<span class="input-group-btn">
+							<button class="btn btn-default show-password">Show</button>
+						</span>
+					</div>
+				</h5>
+				<h5>
+					<div></div>
+					<div class="input-group">
+						<span class="input-group-addon">
+						   <i class="fa fa-tasks fa-fw"></i>
+						</span>
+						<input type="text" placeholder="Database" class="form-control login-database">
+					</div>
+				</h5>
+			</div>
+			<div class="modal-footer">
+				<i class="fa fa-circle-o-notch fa-spin fa-fw login-spinner" style="display: none;"></i>
+				<button class="btn btn-success login-save">Save</button>
+				<button class="btn btn-alert modal-close">Close</button>
+			</div>
+		</div>
+	</div>
+</div> <!--/login form-->
+<div class="modal fade about-form" tabindex="-1">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1>About</h1>
+			</div> <!--/modal-head-->
+			<div class="modal-body">
+				FooBar
+			</div> <!--/modal-body-->
+			<div class="modal-footer">
+				<button class="btn btn-default modal-close">Close</button>
+			</div> <!--/modal-foot-->
+		</div>
+	</div>
+</div> <!--/about form-->
+<div class="modal fade contact-form" tabindex="-1">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h1>Contact</h1>
+			</div> <!--/modal-head-->
+			<div class="modal-body">
+				FooBar
+			</div> <!--/modal-body-->
+			<div class="modal-footer">
+				<button class="btn btn-default modal-close">Close</button>
+			</div> <!--/modal-foot-->
+		</div>
+	</div>
+</div> <!--/contact form-->
